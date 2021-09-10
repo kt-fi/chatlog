d11b62307338d4839b84770ca781ec5664d48499
Christopher Hillman <katie5five.5@gmail.com>
2021-09-10 08:52:57 +0200

    added route for downloading chat history

--- serverResponse.js
+++ serverResponse.js
@@ -1,18 +1,21 @@
 let ChatMessage = require("./models/chatModel")
 
-let questions = [
-    {q: 1,
-    question: "What is your email?"},
-    {q: 2,
-    question: "What would you like to talk about?",
-     answers: ["Our Products","Our Services","Work With Us"]},
-    {q: 3,
-    question: "When should we contact you?",
-    answers: ["Monday","Tuesday","Wednesday","Thursday", "Friday"]},
-    {q: 4,
-        question: "Thankyou we will be in touch shortly!!"
-        }
-]
+// let questions = [
+//     {q: 1,
+//     question: "What is your email?"},
+//     {q: 2,
+//     question: "What would you like to talk about?",
+//      answers: ["Our Products","Our Services","Work With Us"]},
+//     {q: 3,
+//     question: "When should we contact you?",
+//     answers: ["Monday","Tuesday","Wednesday","Thursday", "Friday"]},
+//     {q: 4,
+//         question: "Thankyou we will be in touch shortly!!"
+//         }
+// ]
+
+
+
 let i = 0;
 let askQuestion = async () => {
   
