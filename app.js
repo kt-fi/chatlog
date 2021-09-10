59300185d9d4a58b379712fe5840a7fd9a39cdca
Christopher Hillman <katie5five.5@gmail.com>
2021-09-10 03:31:58 +0200

    server set up with middlewares

---
+++ app.js
@@ -0,0 +1,36 @@
+let express = require("express");
+let app = express();
+let http = require("http").Server(app);
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+let cors = require("cors");
+let socket = require('socket.io')(http);
+
+//Middlewares
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(cors());
+app.use(express.static(__dirname + "/public"))
+
+
+// Set Up and connect to Database
+let dbUrl = "mongodb://localhost:27017/chatDev"
+
+mongoose.connect(dbUrl, ()=>{
+    console.log("Mongo db connected");
+})
+
+
+//Get Home Page
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+
+
+
+
+// Listen for server port
+http.listen(9090, ()=>{
+    console.log("Listening on Port: 9090");
+})
